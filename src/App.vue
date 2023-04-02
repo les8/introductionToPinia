02b04c46c325d8479128ec0d3c408a8288233c37
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-04-02 11:20:48 +0300

    Заготовка для App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,50 @@
+<template>
+  <header class="header">
+    <img src="../public/logo.svg" alt="logo" class="header-logo">
+    <h2>My favorite movies</h2>
+  </header>
+  <div class="movies">
+    
+  </div>
+</template>
+
 <script setup></script>
 
-<template></template>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
 
-<style scoped></style>
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+
+.btn:hover {
+  opacity: 0.7;
+}
+
+.btn_green {
+  background: #37df5c;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
